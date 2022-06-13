52e90e2f6c732d0435d41c57bdde2ba05b07850d
Andie Johnson <ajohnson2@naic.org>
2022-06-13 12:38:37 -0500

    added a todo

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -48,4 +48,6 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    // TODO - stopped at bonus mission # 2, complete the rest
+
 }
